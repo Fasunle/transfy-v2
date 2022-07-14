dc410841dc017180d59b6f0c0c0462abf6141c3d
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-14 18:29:08 +0100

    feat: use user store

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     </ul>
 
     <div class="flex items-center justify-center">
-      <span>John Doe</span>
+      <span>{{store.user.username}}</span>
       <div class="ml-4">
         <img class="w-16 h-16 rounded-full p-4" src="./assets/user.png" alt="User">
       </div>
@@ -22,8 +22,42 @@
 
 <script>
 
+import {useUserStore} from "./store";
+
 export default {
   name: 'App',
+  setup(){
+    const store = useUserStore();
+
+    // mock user data
+    const user = {
+                id: "1",
+                session_id_status: "logged",
+                username: "jagjeet-singh",
+                name: "Jagjeet Singh Admin",
+                email: "jagjeet@njgraphica.com",
+                mobile_number: "8972198798",
+                gender: "male",
+                date_of_birth: "1994-12-22",
+                mobile_number_verified_at: null,
+                email_verified_at: "2021-07-02 13:35:42",
+                country_name: "Nigeria",
+                country_id: "1",
+                currency_name: "Nigerian Naira",
+                currency_code: "NGN",
+                country_code: "NG",
+                is_suspended: "no",
+                created_at: "2019-04-09 23:34:50",
+                updated_at: "2022-07-06 15:56:50"
+            }
+    // update the app state
+    // this should be done in the submit button handler on the login page
+    store.loginUser(user);
+
+    return {
+      store
+    }
+  }
 }
 </script>
 
