4c7d1ec5af3fa727668942da0491f82d6bd61291
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-14 18:31:29 +0100

    feat: configure pinia state management

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
+import {createPinia} from "pinia";
 import App from './App.vue';
 import "./assets/index.css";
 import router from './router';
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia();
+
+createApp(App)
+    .use(pinia)     // Pinia state management library
+    .use(router)
+    .mount('#app')
