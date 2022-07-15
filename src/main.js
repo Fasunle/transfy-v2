9896ab1941643afcffd90ad64777fe61e266b467
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-15 14:14:59 +0100

    feat: Auth0 configuration

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from 'vue';
 import {createPinia} from "pinia";
+import { createAuth0 } from '@auth0/auth0-vue';
 import App from './App.vue';
 import "./assets/index.css";
 import router from './router';
 
 const pinia = createPinia();
+const auth = createAuth0({
+    domain: process.env.VUE_AUTH_DOMAIN,
+    client_id: process.env.VUE_CLIENT_ID,
+    redirect_uri: window.location.origin
+});
 
 createApp(App)
+    .use(auth)
     .use(pinia)     // Pinia state management library
     .use(router)
     .mount('#app')
