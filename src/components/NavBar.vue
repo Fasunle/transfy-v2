d057e40799c2426650e45232c1cc0bb1a2391ab6
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-15 14:44:48 +0100

    feat: Navigation html tags

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <nav>
+        <div>
+            <div>
+                <img src="../assets/menu-icon.svg" alt="menu icon">
+            </div>
+            <h1>Transfy</h1>
+        </div>
+        <ul>
+            <li>
+                <router-link to="/">Dashboard</router-link>
+            </li>
+            <li>
+                <router-link to="/login">Login</router-link>
+            </li>
+            <li>
+                <router-link to="/logout">Logout</router-link>
+            </li>
+        </ul>
+        <div>
+            <img src="../assets/user.png" alt="Current User">
+        </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
