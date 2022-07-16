e107b9b212b6bbaebb5c96006eb176459604708c
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-16 16:40:00 +0100

    feat: delete margin to prevent overflow

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <aside>
       <nav>
-        <ul class="sidebar w-32 p-4 flex flex-col h-64 justify-start mt-16 items-center text-gray-600">
+        <ul class="sidebar w-32 p-4 flex flex-col h-64 justify-start items-center text-gray-600">
 
           <li class="p-2 mt-2 bg-yellow-300 hover:bg-yellow-400 w-full rounded-md"><router-link to="/dashboard">Dashboard</router-link></li>
 
@@ -10,7 +10,7 @@
           <li class="p-2 mt-2 bg-yellow-300 hover:bg-yellow-400 w-full rounded-md"><router-link  to="/balances">Balances</router-link></li>
 
           <li class="p-2 mt-2 bg-yellow-300 hover:bg-yellow-400 w-full rounded-md"><router-link  to="/orders">Orders</router-link></li>
-          
+
           <li class="p-2 mt-2 bg-yellow-300 hover:bg-yellow-400 w-full rounded-md"><router-link  to="/get-info">Get-Info</router-link></li>
         </ul>
       </nav>
