7b43005613f02e81185145570ca4bfd5f6746109
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-16 17:46:06 +0100

    feat: App router with components

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,43 @@
 
 import { createRouter, createWebHistory } from "vue-router";
 import Signin from "@/components/Signin.vue";
-import Dashboard from "@/components/Dashboard.vue";
+import Balances from "@/components/dashboard/Balances.vue";
+import Countries from "@/components/dashboard/Countries.vue";
+import Orders from "@/components/dashboard/Orders.vue";
+import GetInfo from "@/components/dashboard/GetInfo.vue";
+import Dashboard from "@/components/dashboard";
 
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        component: Dashboard
-    },
     {
         path: "/signin",
         name: "Sign in",
         component: Signin
-    }
+    },
+    {
+        path: "/dashboard",
+        name: "Dashboard",
+        component: Dashboard
+    },
+    {
+        path: "/countries",
+        name: "Countries",
+        component: Countries
+    },
+    {
+        path: "/orders",
+        name: "Orders",
+        component: Orders
+    },
+    {
+        path: "/get-info",
+        name: "Get Info",
+        component: GetInfo
+    },
+    {
+        path: "/balances",
+        name: "Balances",
+        component: Balances
+    },
 ];
 
 const router = createRouter({
