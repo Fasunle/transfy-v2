fa6431e5ee9ee86f565afa4ddc55566f5b3d435b
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-17 23:48:24 +0100

    feat: Order pane of the dashboard

--- src/components/dashboard/index.vue
+++ src/components/dashboard/index.vue
@@ -1,15 +1,52 @@
 <template>
-    <div>
-        Dashboard
+    <div class="p-4">
+        <h1 class="text-center text-3xl font-sans font-medium my-4">Dashboard</h1>
+
+        <div class="dashboard grid grid-cols-12 grid-rows-6 gap-4">
+
+            <div class="col-start-1 col-span-4 row-span-full rounded shadow px-4 max-h-full overflow-y-scroll snap-center">
+                <h2 class="text-center text-xl font-sans font-medium my-4">Orders</h2>
+
+                <Order
+                    v-for="order in orders"
+                    :key="order.id"
+                    :order="order"
+                    class="mx-auto"
+                />
+            </div>
+            <div class="col-start-5 col-span-full row-span-full rounded shadow px-4 max-h-full">
+                
+            </div>
+
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import {useStore} from '@/store';
+import Order from './Order.vue';
+import {orders, } from "../../data.json";
 
+
+export default {
+    components: { Order },
+    setup(){
+
+        const store = useStore()
+
+        // fetch orders and update global state
+        store.setOrders(orders);
+
+        return {
+            orders: store.orders
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+    .dashboard{
+        min-height: calc(100vh - 12rem);
+        max-height: calc(100vh - 12rem);
+    }
 </style>
