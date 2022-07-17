a9797ba1a82c1387b3d950cd088493a5dbbbb183
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-17 17:17:59 +0100

    feat: info store configuration

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,16 @@ export const useStore = defineStore("main", {
     // user state
     state: () => {
         return {
-            countries: []
+            countries: [],
+            info: []
         }
     },
     actions: {
         setCountries(countries){
             this.countries = countries;
-        }
+        },
+        setInfo(info){
+            this.info = info;
+        },
     }
 });
